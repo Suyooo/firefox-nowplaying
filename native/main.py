77536874a5385e84b7939d1c5009867e6f15f017
Suyooo <me@suyo.be>
2025-05-02 22:17:34 +0200

    native: UI server section

--- native/main.py
+++ native/main.py
@@ -8,13 +8,16 @@ from receiver import handle as receiver_handle
 
 
 if __name__ == '__main__':
+	config = {
+		"format": "{$title$}{$if_artist$ - $}{$artist$}",
+		"server_port": 8944,
+	}
 	if os.path.isfile(os.path.join(os.path.dirname(__file__), "settings.json")):
 		with open(os.path.join(os.path.dirname(__file__), "settings.json"), "r") as configfile:
-			config = json.load(configfile)
-	else:
-		config = {
-			"format": "{$title$}{$if_artist$ - $}{$artist$}",
-		}
+			try:
+				config = config | json.load(configfile)
+			except Exception:
+				pass
 
 	if len(sys.argv) == 1:
 		# Config UI
--- native/ui.py
+++ native/ui.py
@@ -8,20 +8,37 @@ import tkinter.filedialog as tkfd
 import tkinter.ttk as ttk
 from server import run_server
 
-var_path = None
-var_html = None
 var_format = None
+var_port = None
+var_url = None
+var_status = None
+
+
+class EntryNumber(ttk.Entry):
+	def __init__(self, master=None, textvariable=None, **kwargs):
+		tk.Entry.__init__(self, master, textvariable=textvariable, **kwargs)
+		self.old_value = textvariable.get()
+		self.var = textvariable
+		textvariable.trace('w', self.check)
+
+	def check(self, *args):
+		if self.var.get().isdigit(): 
+			self.old_value = self.var.get()
+		else:
+			self.var.set(self.old_value)
 
 
 def handle(config):
-	global var_path, var_html, var_format
+	global var_format, var_port, var_url, var_status
 
 	root = tk.Tk()
 	root.title("Now Playing Config")
 
-	# var_path = tk.StringVar(value=config["file_path"])
-	# var_html = tk.BooleanVar(value=config["wrap_html"])
 	var_format = tk.StringVar(value=config["format"])
+	var_port = tk.StringVar(value=config["server_port"])
+
+	var_url = tk.StringVar(value="")
+	var_status = tk.StringVar(value="Server is offline.")
 
 	frm_install = tk.LabelFrame(text="Install in Firefox", relief=tk.GROOVE, borderwidth=1, padx=10, pady=5)
 	frm_install.columnconfigure(0, weight=1)
@@ -32,22 +49,9 @@ def handle(config):
 	btn_uninstall.grid(column=1, row=0, padx=5, sticky="we")
 	frm_install.pack(fill=tk.X, padx=10, pady=5)
 
-	# frm_path = tk.LabelFrame(text="Choose File Path", relief=tk.GROOVE, borderwidth=1, padx=10, pady=5)
-	# frm_path.columnconfigure(0, weight=3)
-	# frm_path.columnconfigure(1, weight=1)
-	# etr_path = ttk.Entry(master=frm_path, textvariable=var_path)
-	# etr_path.grid(column=0, row=0, padx=5, sticky="we")
-	# btn_path = ttk.Button(master=frm_path, text="Browse...", command=click_browse)
-	# btn_path.grid(column=1, row=0, padx=5, sticky="we")
-	# frm_path.pack(fill=tk.X, padx=10, pady=5)
-
 	frm_output = tk.LabelFrame(text="Text Format", relief=tk.GROOVE, borderwidth=1, padx=10, pady=5)
-	# chk_html = ttk.Checkbutton(master=frm_output, text="Output HTML for use with Browser Source", variable=var_html)
-	# chk_html.pack(fill=tk.X, padx=5, pady=5)
-
 	etr_format = ttk.Entry(master=frm_output, textvariable=var_format)
 	etr_format.pack(fill=tk.X, padx=5, pady=5)
-
 	frm_help = tk.Frame(master=frm_output)
 	lbl_help_al = ttk.Label(master=frm_help, text="{$title$}", font=("monospace", 12, "bold"))
 	lbl_help_al.grid(column=0, row=0, padx=5, sticky="nw")
@@ -67,11 +71,31 @@ def handle(config):
 	frm_help.pack(fill=tk.X, pady=5)
 	frm_output.pack(fill=tk.X, padx=10, pady=5)
 
+	frm_server = tk.LabelFrame(text="Page Server", relief=tk.GROOVE, borderwidth=1, padx=10, pady=5)
+	frm_server.columnconfigure(0, weight=1, uniform='half')
+	frm_server.columnconfigure(1, weight=1, uniform='half')
+	frm_port = tk.Frame(master=frm_server)
+	lbl_port = ttk.Label(master=frm_port, text="Port")
+	lbl_port.pack(side=tk.LEFT, padx=5)
+	etr_port = EntryNumber(master=frm_port, textvariable=var_port)
+	etr_port.pack(side=tk.LEFT, padx=5, fill=tk.X, expand=True)
+	btn_serve = ttk.Button(master=frm_port, text="Start Server", command=click_serve)
+	btn_serve.pack(side=tk.LEFT, padx=5, fill=tk.X, expand=True)
+	frm_port.grid(column=0, row=0, sticky="we")
+	frm_url = tk.Frame(master=frm_server)
+	lbl_url = ttk.Label(master=frm_url, text="URL")
+	lbl_url.pack(side=tk.LEFT, padx=5)
+	etr_url = ttk.Entry(master=frm_url, textvariable=var_url, state="readonly")
+	etr_url.pack(side=tk.LEFT, padx=5, fill=tk.X, expand=True)
+	btn_copy = ttk.Button(master=frm_url, text="Copy", command=click_copy)
+	btn_copy.pack(side=tk.LEFT, padx=5, fill=tk.X, expand=True)
+	frm_url.grid(column=1, row=0, sticky="we")
+	lbl_status = ttk.Label(master=frm_server, textvariable=var_status)
+	lbl_status.grid(column=0, columnspan=2, row=1, padx=5, sticky="we")
+	frm_server.pack(fill=tk.X, padx=10, pady=5)
+
 	btn_save = ttk.Button(text="Save Configuration", command=click_save)
 	btn_save.pack(fill=tk.X, padx=10, pady=10)
-
-	btn_serve = ttk.Button(text="Serve Page", command=click_serve)
-	btn_serve.pack(fill=tk.X, padx=10, pady=10)
 
 	root.mainloop()
 
@@ -130,10 +154,15 @@ def click_uninstall():
 
 
 def click_serve():
+	var_url.set("http://localhost:" + var_port.get() + "/nowplaying.html")
 	print("Starting server...")
-	thread = threading.Thread(target=run_server, args=(8944, ))
+	thread = threading.Thread(target=run_server, args=(int(var_port.get()), ))
 	thread.setDaemon(True)
 	thread.start()
+
+
+def click_copy():
+	print("TODO...")
 
 
 def click_save():
@@ -141,6 +170,7 @@ def click_save():
 		with open(os.path.join(os.path.dirname(__file__), "settings.json"), "w") as configfile:
 			json.dump({
 				"format": var_format.get(),
+				"server_port": int(var_port.get()),
 			}, configfile, indent="\t")
 		tk.messagebox.showinfo(title="Now Playing Config", message="Configuration has been saved!")
 	except Exception as e:
