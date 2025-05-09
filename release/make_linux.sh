14df88676fcb6a132dfaf23d918e8b9dc1da4b3c
Suyooo <me@suyo.be>
2025-05-09 16:15:24 +0200

    native: Bump version

--- release/make_linux.sh
+++ release/make_linux.sh
@@ -14,6 +14,6 @@ nuitka ../native/__main__.py --onefile --enable-plugin=tk-inter \
 	--include-module=receiver --include-module=ui \
 	--include-data-file=../native/native-base.json=ui/native-base.json \
 	--include-data-file=../native/nowplaying-template.html=receiver/nowplaying-template.html \
-	"--product-name=Now Playing" --file-version=1.2.0 --file-description=https://github.com/Suyooo/firefox-nowplaying \
+	"--product-name=Now Playing" --file-version=1.3.0 --file-description=https://github.com/Suyooo/firefox-nowplaying \
 	--linux-icon=../addon/icon-notification.png  --output-filename=$FILE
 mkdir -p dist && mv $FILE dist
